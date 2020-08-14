bf7bc03e8110e63a3b37ac213e8cd6319466c0fd
ryusei.takebe <null.mainph@gmail.com>
2020-08-14 06:38:06 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Sheduling Management Service</h1>
-        </div>
-        <div>
-            <!-- bootstrapでレイアウトを工夫する -->
-            {{-- ユーザ登録ページへのリンク --}}
-            <button>{!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}</button>
-            {{-- ログインページへのリンク --}}
-            <button><a href="#" class="">ログイン</a></botton>
-        </div>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Sheduling Management Service</h1>
+            </div>
+            <div>
+                <!-- bootstrapでレイアウトを工夫する -->
+                {{-- ユーザ登録ページへのリンク --}}
+                <button>{!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}</button>
+                {{-- ログインページへのリンク --}}
+                <button>{!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-secondary']) !!}</botton>
+            </div>
         
-        <div class="text-center">
-            <!-- サービスの概要説明記入部分 -->
-        </div>
+            <div class="text-center">
+                <!-- サービスの概要説明記入部分 -->
+            </div>
         
-        <div>
-            <!-- bootstrapでレイアウトを工夫する -->    
-            {{-- ユーザ登録ページへのリンク --}}
-            <button>{!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}</button>
-            {{-- ログインページへのリンク --}}
-            <button><a href="#" class="">ログイン</a></botton>
+            <div>
+                <!-- bootstrapでレイアウトを工夫する -->    
+                {{-- ユーザ登録ページへのリンク --}}
+                <button>{!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}</button>
+                {{-- ログインページへのリンク --}}
+                <button>{!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-secondary']) !!}</botton>
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
